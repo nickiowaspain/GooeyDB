f13430e5b9d5a1424dec8e78f97032ac47610984
Joseph Kim <canyouspelljoe@gmail.com>
2017-01-04 14:51:03 -0800

    server.js sample db route and model db route sample and models folder updated

---
+++ models/model.js
@@ -0,0 +1,6 @@
+// const retrieve = require('./retrieve_method');
+
+module.exports = {
+  // retrieve : retrieve.retrieve,
+
+};
--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const models = require('./models/model');
 
 const app = express();
 const PORT = 3000;
@@ -10,5 +11,7 @@ const PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'app')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
+
+// app.get('/db', models.retrieve);
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
