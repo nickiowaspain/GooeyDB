59fedc629b9dcbc77c601e5994da3fdc2302da3a
Joseph Kim <canyouspelljoe@gmail.com>
2017-01-04 22:35:48 -0800

    temp commit

--- app/directives/tableSelector.js
+++ app/directives/tableSelector.js
@@ -1,8 +1,33 @@
 angular.module('TableSelector', ['databaseFactory'])
   .controller('tableSelector', ['$scope', '$http', 'databaseFactory', function($scope, $http, databaseFactory) {
+    
+    $scope.$watch(function() {return databaseFactory.dbName}, function() {
+
+      if (databaseFactory.dbName) {
+        // create database url string from databaseFactory being updated
+        var url = 'postgres://' + databaseFactory.user +':' + databaseFactory.pass + '@' + databaseFactory.url + ':5432/' +databaseFactory.dbName;
+
+        var data = {
+          url : url
+        }
+        
+        $http.post('/getTables', data).then(function(res) {
+          var tables = res.data;
+          console.log('response:', tables);
+        });
+      }
+    })
+
+    // example tableObj
+    $scope.tableObj = {
+      students : 'students',
+      tutors : 'tutors'
+    };
 
     // GET request for table triggered by click
     $scope.getTable = function(tName) {
+      // watch for change in databaseFactory to update list of tables in selected database
+
       // get database url
       $scope.url = 'postgres://aeqxadhz:qHz6IxCJsV2GXmiQRzEPTU_wj4WufZQh@elmer.db.elephantsql.com:5432/aeqxadhz';
       
@@ -10,7 +35,7 @@ angular.module('TableSelector', ['databaseFactory'])
       $scope.tableName = tName;
 
       // put database url and tablename into an object for the POST request
-      data = {
+      var data = {
         url: $scope.url,
         tableName: $scope.tableName
       };
