0a60d14b24af1aa329e96bb049330fc7d0f90f6c
Jimmy Huynh <jkchuynh829@gmail.com>
2017-01-04 19:49:35 -0800

    db selector styling and logic no cookies yet

--- app/directives/databaseSelector.js
+++ app/directives/databaseSelector.js
@@ -1,6 +1,14 @@
 angular
 .module('DatabaseSelector', [])
 .controller('selectCtrl', function($scope) {
+  $scope.databases = [
+    {
+      dbName: 'aeqxadhz',
+      user: 'aeqxadhz',
+      password: 'qHz6IxCJsV2GXmiQRzEPTU_wj4WufZQh',
+      url: 'elmer.db.elephantsql.com'
+    }
+  ];
   $scope.dbs = ['database 1', 'database 2', 'database 5', 'orange'];
   $selectedTestAccount = $scope.dbs[1];
   $scope.active = '';
