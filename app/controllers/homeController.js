e527f3fef269b77ce79e7b85e1eef090a8836929
Jimmy Huynh <jkchuynh829@gmail.com>
2017-01-04 23:55:24 -0800

    homectrl and partial testing

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -2,7 +2,22 @@ angular
   .module('HomeController', ['ngRoute'])
   .controller('HomeController', HomeController);
 
-
-function HomeController($scope) {
-  $scope.name = 'Jimmy';
+function HomeController($scope, $http) {
+  $scope.dbSettings = {
+    username: 'aeqxadhz',
+    password: 'qHz6IxCJsV2GXmiQRzEPTU_wj4WufZQh',
+    url: 'elmer.db.elephantsql.com',
+    dbname: 'aeqxadhz'
+  };
+  $scope.serverConnect = function() {
+    $http({
+      url: 'http://localhost:3000/getTables',
+      method: 'POST',
+      data: $scope.dbSettings,
+    }).then(function (httpResponse) {
+      console.log('HELLOOOOO', httpResponse)
+    }).catch(function(err) {
+      console.log('NOOOOOOOOOOOO!');
+    });
+  }
 }
--- server.js
+++ server.js
@@ -9,10 +9,24 @@ const Sequelize = require('sequelize');
 const app = express();
 const PORT = 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'app')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
+
+app.post('/getTables', function(req, res) {
+  const data = req.body;
+  const sequelize = new Sequelize(`postgres://${data.username}:${data.password}@${data.url}:5432/${data.dbname}`);
+
+  sequelize
+    .authenticate()
+    .then(function(err) {
+      
+    })
+    .catch(function (err) {
+      console.log('Unable to connect to the database:', err);
+    });
+});
 
 app.post('/connect', function (req, res, done) {
   var data = req.body;
