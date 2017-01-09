497d97e5803fb00b1fbdfc1bb89f468b23a2c487
Mike Sherman <himikesherman@gmail.com>
2017-01-09 12:28:11 -0800

    maps enrichment

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -2,6 +2,26 @@ angular
   .module('HomeController', ['ngRoute', 'rowFactory', 'databaseFactory', 'sortFactory'])
   .controller('HomeController', ['$scope', '$window', 'rowFactory', 'databaseFactory', 'sortFactory','$http', '$sce',HomeController]); 
 
+function checkAddress(potential) {
+  if (potential) {
+    potential = potential.split(' ');
+
+    //an address needs a street number and name, state and zip code at a bare minimum
+    if (potential.length > 3) {
+      let streetNumber = parseInt(potential[0], 10);
+      let streetName = potential[1];
+      let state = potential[potential.length - 2];
+      let zip = parseInt(potential[potential.length - 1], 10);
+
+      if (!isNaN(streetNumber) && streetName && state.length === 2 && !isNaN(zip)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }  
+}
+
 function HomeController($scope, $window, rowFactory, databaseFactory, sortFactory, $http, $sce) {
   // creates and watches changes rowfactory for changes to the database records and columns
   $scope.rows = rowFactory.rows;
@@ -15,9 +35,9 @@ function HomeController($scope, $window, rowFactory, databaseFactory, sortFactor
           // enrichment functions
         if(rowFactory.rows[row][column].match(/\.(jpg|png|gif)/)) {
           temp.push({
-          type:'image',
-          resource: rowFactory.rows[row][column]
-          })
+            type:'image',
+            resource: rowFactory.rows[row][column]
+          });
           
         } else if (rowFactory.rows[row][column].match(/(youtube.com|youtu.be)/g)) {
           let embedLink = rowFactory.rows[row][column].replace(/watch\?v=/, "embed/")
@@ -26,7 +46,13 @@ function HomeController($scope, $window, rowFactory, databaseFactory, sortFactor
           temp.push({
             type:'youtube',
             resource: $sce.trustAsResourceUrl(embedLink + "?autoplay=0&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&origin=https://youtubeembedcode.com")
-          })
+          });
+        } else if (checkAddress(rowFactory.rows[row][column])) {
+          temp.push({
+            type:'map',
+            resource: 'https://maps.google.com?q=' + rowFactory.rows[row][column]
+            //resource: 'https://maps.googleapis.com/maps/api/staticmap?center=' + rowFactory.rows[row][column] + '&zoom=15&scale=false&size=400x200&maptype=roadmap&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:%7C' + rowFactory.rows[row][column]
+          });
         } else {
           temp.push({});
         }
